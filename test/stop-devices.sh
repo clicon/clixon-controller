9c3cc06b09d85e337ffb3352b6e3d044b397207b
Olof hagsand <olof@hagsand.se>
2023-02-20 13:48:04 +0100

    CI: ldconfig + stop-devices

---
+++ test/stop-devices.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Stop clixon example container devices and initiate with config x=11, y=22
+set -eux
+
+# Number of device containers to start
+: ${nr:=2}
+
+IMG=clixon-example
+
+for i in $(seq 1 $nr); do
+    NAME=$IMG$i
+    sudo docker kill $NAME || true
+done
+
