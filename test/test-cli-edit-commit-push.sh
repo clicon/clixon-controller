057ab9ba6437edc8570db9faf70ad1aec70f4b5a
Kristofer Hallin <kristofer@sunet.se>
2023-04-06 09:03:00 +0200

    Sleep less.

--- test/test-cli-edit-commit-push.sh
+++ test/test-cli-edit-commit-push.sh
@@ -37,7 +37,5 @@ expectpart "$(${PREFIX} $clixon_cli -1 -f $CFG -m configure commit)" 0 ""
 new "CLI: Show configuration"
 expectpart "$(${PREFIX} $clixon_cli -1 -f $CFG show configuration cli)" 0 "^services test cli_test" "^services test cli_test parameter XXX" "^services test cli_test parameter XXX value 1.2.3.4"
 
-sleep 5
-
 new "CLI: Push configuration"
 expectpart "$(${PREFIX} $clixon_cli -1 -f $CFG sync push)" 0 ""
