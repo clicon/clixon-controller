96d0a33c72412407fc980b4c78d56adae9439f57
Kristofer Hallin <kristofer@sunet.se>
2023-04-06 08:34:45 +0200

    New simplified service YANG for Clixon example.

--- test/py/clixon_example.py
+++ test/py/clixon_example.py
@@ -1,16 +1,11 @@
 from clixon.clixon import rpc
 
-# Test
 
 @rpc()
 def setup(root, log):
     for device in root.devices.device:
-        device.root.delete("table")
-        device.root.create("table", attributes={"xmlns": "urn:example:clixon"})
-
         for service in root.services.test:
-            parameter = service.table.parameter
-
+            parameter = service.parameter
             device.root.table.add(parameter)
 
 
