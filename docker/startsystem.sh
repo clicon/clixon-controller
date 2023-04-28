34d27719f1b67ebfc97c55b89c0c9327b234f8d3
Kristofer Hallin <kristofer@sunet.se>
2023-04-28 12:43:36 +0000

    Dockerfile etc for clixon-controller.

--- docker/startsystem.sh
+++ docker/startsystem.sh
@@ -39,9 +39,12 @@ DBG=${DBG:-0}
 # sudo: setrlimit(RLIMIT_CORE): Operation not permitted
 echo "Set disable_coredump false" > /etc/sudo.conf
 
+# Start PyAPI
+python3 /usr/local/bin/clixon_server.py -m /clixon/clixon-pyapi/modules/ -d -F &
+
 # Start clixon backend
 >&2 echo "start clixon_backend:"
-/usr/local/sbin/clixon_backend -FD $DBG -f /usr/local/etc/clixon.xml -l e # logs on docker logs
+/usr/local/sbin/clixon_backend -FD $DBG -f /usr/local/etc/controller.xml -l e # logs on docker logs
 
 # Alt: let backend be in foreground, but then you cannot restart
 /bin/sleep 100000000
