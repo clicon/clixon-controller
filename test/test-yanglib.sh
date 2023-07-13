f0c946db3ba253af9dde636d8a81b186036d144e
Olof hagsand <olof@hagsand.se>
2023-07-13 14:36:42 +0200

    Adapt to new clixon yang version

--- test/test-yanglib.sh
+++ test/test-yanglib.sh
@@ -91,7 +91,9 @@ if [ -n "$match" ]; then
     exit 1
 fi
 
-expected='<devices xmlns="http://clicon.org/controller"><device><config><yang-library xmlns="urn:ietf:params:xml:ns:yang:ietf-yang-library"><module-set><name>mount</name><module><name>clixon-autocli</name><revision>2022-02-11</revision><namespace>http://clicon.org/autocli</namespace></module>'
+#expected='<devices xmlns="http://clicon.org/controller"><device><config><yang-library xmlns="urn:ietf:params:xml:ns:yang:ietf-yang-library"><module-set><name>mount</name><module><name>clixon-autocli</name><revision>2022-02-11</revision><namespace>http://clicon.org/autocli</namespace></module>'
+expected='<devices xmlns="http://clicon.org/controller"><device><config><yang-library xmlns="urn:ietf:params:xml:ns:yang:ietf-yang-library"><module-set><name>mount</name><module><name>clixon-autocli</name>'
+
 match=$(echo $ret | grep --null -Eo "$expected") || true
 if [ -z "$match" ]; then
     echo "netconf unexpected yang-library"
