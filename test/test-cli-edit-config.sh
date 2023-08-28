ce0fd7a343a9ead0286387c0bb7a24076d897a44
Kristofer Hallin <kristofer@sunet.se>
2023-08-28 11:46:28 +0000

    OpenConfig containers should be named openconfig1 and openconfig2.

--- test/test-cli-edit-config.sh
+++ test/test-cli-edit-config.sh
@@ -29,48 +29,48 @@ new "reset controller"
 
 # new "Close all devices"
 expectpart "$($clixon_cli -1 -f $CFG connection close)" 0 ""
-expectpart "$($clixon_cli -1 -f $CFG show devices)" 0 "r1.*CLOSED" "r2.*CLOSED"
+expectpart "$($clixon_cli -1 -f $CFG show devices)" 0 "openconfig1.*CLOSED" "openconfig2.*CLOSED"
 
 # new "Connect to devices"
 expectpart "$($clixon_cli -1 -f $CFG connection open)" 0 ""
 
 # new "Verify devices are open"
 sleep 10
-expectpart "$($clixon_cli -1 -f $CFG show devices)" 0 "r1.*OPEN" "r2.*OPEN"
+expectpart "$($clixon_cli -1 -f $CFG show devices)" 0 "openconfig1.*OPEN" "openconfig2.*OPEN"
 
 # new "Reconnect to devices"
 expectpart "$($clixon_cli -1 -f $CFG connection reconnect)" 0 ""
 
 # new "Verify devices are open"
 sleep 10
-expectpart "$($clixon_cli -1 -f $CFG show devices)" 0 "r1.*OPEN" "r2.*OPEN"
+expectpart "$($clixon_cli -1 -f $CFG show devices)" 0 "openconfig1.*OPEN" "openconfig2.*OPEN"
 
 new "Show device diff, should be empty"
 expectpart "$($clixon_cli -1 -f $CFG show devices diff)" 0 ""
 expectpart "$($clixon_cli -1 -f $CFG 'show devices r* diff')" 0 ""
-expectpart "$($clixon_cli -1 -f $CFG show devices r1 diff)" 0 ""
-expectpart "$($clixon_cli -1 -f $CFG show devices r2 diff)" 0 ""
+expectpart "$($clixon_cli -1 -f $CFG show devices openconfig1 diff)" 0 ""
+expectpart "$($clixon_cli -1 -f $CFG show devices openconfig2 diff)" 0 ""
 
-new "Configure hostname on r1"
-expectpart "$($clixon_cli -1 -f $CFG -m configure set devices device r1 config system config hostname test1)" 0 ""
+new "Configure hostname on openconfig1"
+expectpart "$($clixon_cli -1 -f $CFG -m configure set devices device openconfig1 config system config hostname test1)" 0 ""
 
-new "Verify show compare on r1"
-expectpart "$($clixon_cli -1 -f $CFG -m configure show compare)" 0 "^-               hostname r1;" "^\+               hostname test1;"
+new "Verify show compare on openconfig1"
+expectpart "$($clixon_cli -1 -f $CFG -m configure show compare)" 0 "^-               hostname openconfig1;" "^\+               hostname test1;"
 
-new "Verify commit diff on r1"
-expectpart "$($clixon_cli -1 -f $CFG -m configure commit diff)" 0 "^-    <hostname>r1</hostname>" "^\+    <hostname>test1</hostname>"
+new "Verify commit diff on openconfig1"
+expectpart "$($clixon_cli -1 -f $CFG -m configure commit diff)" 0 "^-    <hostname>openconfig1</hostname>" "^\+    <hostname>test1</hostname>"
 
-new "Rollback hostname on r1"
+new "Rollback hostname on openconfig1"
 expectpart "$($clixon_cli -1 -f $CFG -m configure rollback)" 0 ""
 
 new "Configure hostnames on r*"
 expectpart "$($clixon_cli -1 -f $CFG -m configure 'set devices device r* config system config hostname test')" 0 ""
 
 new "Verify show compare on r*"
-expectpart "$($clixon_cli -1 -f $CFG -m configure show compare)" 0 "^-               hostname r1;" "^\+               hostname test;" "^-               hostname r2;"
+expectpart "$($clixon_cli -1 -f $CFG -m configure show compare)" 0 "^-               hostname openconfig1;" "^\+               hostname test;" "^-               hostname openconfig2;"
 
 new "Verify commit diff on r*"
-expectpart "$($clixon_cli -1 -f $CFG -m configure commit diff)" 0 "^-    <hostname>r1</hostname>" "^\+    <hostname>test</hostname>" "^-    <hostname>r2</hostname>"
+expectpart "$($clixon_cli -1 -f $CFG -m configure commit diff)" 0 "^-    <hostname>openconfig1</hostname>" "^\+    <hostname>test</hostname>" "^-    <hostname>openconfig2</hostname>"
 
 new "Rollback hostnames on r*"
 expectpart "$($clixon_cli -1 -f $CFG -m configure rollback)" 0 ""
@@ -86,12 +86,12 @@ for container in $CONTAINERS; do
     expectpart "$(ssh -l $USER $container clixon_cli -1 show configuration cli)" 0 "system config hostname test"
 done
 
-new "Commit hostname on r1"
-expectpart "$($clixon_cli -1 -f $CFG -m configure set devices device r1 config system config hostname r1)" 0 ""
+new "Commit hostname on openconfig1"
+expectpart "$($clixon_cli -1 -f $CFG -m configure set devices device openconfig1 config system config hostname openconfig1)" 0 ""
 expectpart "$($clixon_cli -1 -f $CFG -m configure commit)" 0 ""
 
-new "Commit hostname on r2"
-expectpart "$($clixon_cli -1 -f $CFG -m configure set devices device r2 config system config hostname r2)" 0 ""
+new "Commit hostname on openconfig2"
+expectpart "$($clixon_cli -1 -f $CFG -m configure set devices device openconfig2 config system config hostname openconfig2)" 0 ""
 expectpart "$($clixon_cli -1 -f $CFG -m configure commit)" 0 ""
 
 for container in $CONTAINERS; do
