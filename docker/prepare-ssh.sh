8583389b02e8142bb67e09b82e938753e2b7c9e0
Kristofer Hallin <kristofer@sunet.se>
2023-11-14 09:45:41 +0000

    Make the key installation procedure a bit better.

--- docker/prepare-ssh.sh
+++ docker/prepare-ssh.sh
@@ -4,10 +4,24 @@
 ssh-keygen -q -t rsa -f /root/.ssh/id_rsa -N ""
 
 # Copy SSH key to r1 and r2
-sshpass -p noc ssh-copy-id -o StrictHostKeyChecking=accept-new noc@r1
-sshpass -p noc ssh-copy-id -o StrictHostKeyChecking=accept-new noc@r2
+for i in `seq 3`; do
+    sshpass -p noc ssh-copy-id -o StrictHostKeyChecking=accept-new noc@r1
+    if [ $? == 0 ]; then
+	break
+    fi
+
+    sleep 3
+done
+
+for i in `seq 3`; do
+    sshpass -p noc ssh-copy-id -o StrictHostKeyChecking=accept-new noc@r2
+    if [ $? == 0 ]; then
+	break
+    fi
+
+    sleep 3
+done
 
 # Execute the original entrypoint
 # exec "$@"
 /usr/local/bin/startsystem.sh
-
