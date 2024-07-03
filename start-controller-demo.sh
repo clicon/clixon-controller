935afb74b03ca5ad6dc3dfe7e4e6cdfde8416f3f
Kristofer Hallin <kristofer@sunet.se>
2024-07-03 14:50:01 +0000

    Updated container start scripts.

--- start-controller-demo.sh
+++ start-controller-demo.sh
@@ -2,12 +2,14 @@
 
 docker-compose -f docker/docker-compose-demo.yml up -d
 
+echo ""
+
 # Echo the SSH key to the console
-echo "SSH key for controller:"
+echo "Public SSH key for controller:"
 docker exec -t controller bash -c "cat /root/.ssh/id_rsa.pub"
 
 echo ""
 
 # Echo the CLI command to the console
 echo "To start Controller CLI run:"
-echo "docker exec -it controller clixon_cli -f /usr/local/etc/clixon/controller.xml"
+echo "docker exec -it controller-demo clixon_cli -f /usr/local/etc/clixon/controller.xml"
