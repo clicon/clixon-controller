6a837fe3a798c89dd8c76eb4d65f7c3fd2166c82
Kristofer Hallin <kristofer@sunet.se>
2024-07-03 12:02:50 +0000

    Restructure Docker containers.

--- docker/startsystem.sh
+++ docker/startsystem.sh
@@ -48,8 +48,19 @@ sleep=2
 CONTAINERS="$CONTAINERS"
 EOF
 
+# Generate SSH keys if they don't exist
+if [ ! -f /root/.ssh/id_rsa ]; then
+    >&2 echo "Generating SSH keys:"
+    ssh-keygen -t rsa -b 4096 -f /root/.ssh/id_rsa -N ""
+fi
+
+# Echo the public SSH key
+>&2 echo "Public SSH key generated:"
+cat /root/.ssh/id_rsa.pub
+
+
 # Start clixon backend
->&2 echo "start clixon_backend:"
+>&2 echo "Starting clixon_backend:"
 
 if [ -z ${USE_STARTUPDB+x} ]; then
     /usr/local/sbin/clixon_backend -FD $DBG -f /usr/local/etc/clixon/controller.xml -l e
@@ -59,4 +70,4 @@ else
 fi
 
 # Alt: let backend be in foreground, but then you cannot restart
-/bin/sleep 100000000
+/bin/sleep infinity
