40864ea674a6bd4768d302da5b92f3ecbc108122
Kristofer Hallin <kristofer@sunet.se>
2024-07-04 09:10:19 +0000

    Demo and controller start scripts: Wait five seconds before printing the controller public key.

--- start-controller-demo.sh
+++ start-controller-demo.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
+# Check if any of the containers controller-demo, r1 or r2 are running
+if [ "$(docker ps -q -f name=demo-controller)" ] || [ "$(docker ps -q -f name=r1)" ] || [ "$(docker ps -q -f name=r2)" ]; then
+	echo "Error: Containers controller-demo, r1 or r2 are already running. Please stop them before starting the demo."
+	exit 1
+fi
+
 docker-compose -f docker/docker-compose-demo.yml up -d
 
-echo ""
+sleep 5
 
 # Echo the SSH key to the console
-echo "Public SSH key for controller:"
-docker exec -t controller bash -c "cat /root/.ssh/id_rsa.pub"
-
 echo ""
+echo "Public SSH key for controller:"
+docker exec -t demo-controller bash -c "cat /root/.ssh/id_rsa.pub"
 
 # Echo the CLI command to the console
+echo ""
 echo "To start Controller CLI run:"
 echo "docker exec -it controller-demo clixon_cli -f /usr/local/etc/clixon/controller.xml"
--- start-controller.sh
+++ start-controller.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
+# Check if containers are already running
+if [ "$(docker ps -q -f name=controller)" ]; then
+	echo "Containers are already running. Please stop them first."
+	exit 1
+fi
+
 docker-compose -f docker/docker-compose.yml up -d
 
-echo ""
+sleep 5
 
 # Echo the SSH key to the console
+echo ""
 echo "Public SSH key for controller:"
 docker exec -t controller bash -c "cat /root/.ssh/id_rsa.pub"
 
-echo ""
-
 # Echo the CLI command to the console
+echo ""
 echo "To start Controller CLI run:"
 echo "docker exec -it controller clixon_cli -f /usr/local/etc/clixon/controller.xml"
