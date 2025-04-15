5038315c07e65d7ac65397dd9f4420f70c7d359a
Kristofer Hallin <kristofer@sunet.se>
2025-04-15 13:58:46 +0200

    Revert "Use podman"
    This reverts commit 8f6e96adccdb6a8b4d77af5b5102e8ff0f32d6b4.

--- start-controller-demo.sh
+++ start-controller-demo.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 # Check if any of the containers controller-demo, r1 or r2 are running
-if [ "$(podman ps -q -f name=demo-controller)" ] || [ "$(podman ps -q -f name=r1)" ] || [ "$(podman ps -q -f name=r2)" ]; then
+if [ "$(docker ps -q -f name=demo-controller)" ] || [ "$(docker ps -q -f name=r1)" ] || [ "$(docker ps -q -f name=r2)" ]; then
 	echo "Error: Containers controller-demo, r1 or r2 are already running. Please stop them before starting the demo."
 	exit 1
 fi
 
-podman compose -f docker/docker-compose-demo.yml up -d
+docker compose -f docker/docker-compose-demo.yml up -d
 
 sleep 5
 
 # Echo the SSH key to the console
 echo ""
 echo "Public SSH key for controller:"
-podman exec -t demo-controller bash -c "cat /root/.ssh/id_rsa.pub"
+docker exec -t demo-controller bash -c "cat /root/.ssh/id_rsa.pub"
 
 # Echo the CLI command to the console
 echo ""
 echo "To start Controller CLI run:"
-echo "podman exec -it demo-controller clixon_cli -f /usr/local/etc/clixon/controller.xml"
+echo "docker exec -it demo-controller clixon_cli -f /usr/local/etc/clixon/controller.xml"
