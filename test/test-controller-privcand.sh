f477a4bfc8fa2057ab3ebae1748a8ec313134dd4
Olof hagsand <olof@hagsand.se>
2025-11-06 16:26:19 +0100

    test: disable scrolling prompt in expect script

--- test/test-controller-privcand.sh
+++ test/test-controller-privcand.sh
@@ -188,12 +188,12 @@ set USER [lindex $argv 3]
 
 puts "Spawn First CLI session"
 global session1
-spawn {*}sudo -u $USER clixon_cli -f $CFG -E $CFD -m configure
+spawn {*}sudo -u $USER clixon_cli -f $CFG -E $CFD -m configure -o CLICON_CLI_LINES_DEFAULT=0
 set session1 $spawn_id
 
 puts "Spawn Second CLI session"
 global session2
-spawn {*}sudo -u $USER clixon_cli -f $CFG -E $CFD -m configure
+spawn {*}sudo -u $USER clixon_cli -f $CFG -E $CFD -m configure -o CLICON_CLI_LINES_DEFAULT=0
 set session2 $spawn_id
 
 proc clifn { session command reply } {
